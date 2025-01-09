919ef3c90fad2db60c4e79a0eab3869ef4254e86
bayak <sherine-baya.kochkar@etu.univ-lyon1.fr>
2025-01-09 11:21:59 +0100

    ajout de filtre pour les heures

--- filter.js
+++ filter.js
@@ -30,3 +30,33 @@ fs.readFile('day_S1.json', 'utf8', (err, data) => {
     // Afficher les résultats
     console.log("Données filtrées :", resultatsFiltres);
 });
+const fs = require('fs');
+
+// Charger le fichier JSON nommé 'hour.json'
+const data = JSON.parse(fs.readFileSync('hour.json', 'utf8'));
+
+// Définir les heures de début et de fin pour le filtre
+const startTime = "22:00:00";
+const endTime = "08:00:00";
+
+// Fonction pour filtrer les données sur la base des heures
+function filterByTimeRange(data, startTime, endTime) {
+  return data.filter(entry => {
+    const time = entry.fecha_servidor.split('T')[1]; // Extraire l'heure
+    if (startTime <= endTime) {
+      // Si l'heure de début est avant l'heure de fin (dans la même journée)
+      return time >= startTime && time <= endTime;
+    } else {
+      // Si l'heure de début est après l'heure de fin (traverse la nuit)
+      return time >= startTime || time <= endTime;
+    }
+  });
+}
+
+// Appliquer le filtre
+const filteredData = filterByTimeRange(data, startTime, endTime);
+
+// Sauvegarder les résultats filtrés dans un nouveau fichier JSON
+fs.writeFileSync('filtered_hour.json', JSON.stringify(filteredData, null, 2), 'utf8');
+
+console.log('Les données filtrées ont été sauvegardées dans filtered_hour.json');
